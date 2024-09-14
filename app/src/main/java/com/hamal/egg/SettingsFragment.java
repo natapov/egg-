44da9a51a453068c5d0c84507c93f64b2f3387b1
Shai <shaivopatan@gmail.com>
2024-09-14 13:11:25 +0300

    display current egg ip in settings

--- app/src/main/java/com/hamal/egg/SettingsFragment.java
+++ app/src/main/java/com/hamal/egg/SettingsFragment.java
@@ -1,6 +1,8 @@
 package com.hamal.egg;
 
 import android.os.Bundle;
+
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
@@ -8,5 +10,14 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
+        Preference dynamicValuePref = findPreference("egg_ip");
+        MainActivity ip_provider = (MainActivity) getContext();
+        if (dynamicValuePref != null) {
+            try {
+                dynamicValuePref.setSummary(ip_provider.get_ip());
+            } catch (InterruptedException e) {
+                dynamicValuePref.setSummary("N/A");
+            }
+        }
     }
 }
