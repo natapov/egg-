137ae184a7ac1d0d7f22b0c083c2d3e82b82a166
Shai <shaivopatan@gmail.com>
2024-09-17 16:40:34 +0300

    start adding vlc player, fix nav bar

--- app/src/main/java/com/hamal/egg/ui/PlayerFragment.java
+++ app/src/main/java/com/hamal/egg/ui/PlayerFragment.java
@@ -1,6 +1,7 @@
 package com.hamal.egg.ui;
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,18 +9,22 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-//import org.videolan.libvlc.LibVLC;
-//import org.videolan.libvlc.Media;
-//import org.videolan.libvlc.MediaPlayer;
-//import org.videolan.libvlc.util.VLCVideoLayout;
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+import org.videolan.libvlc.util.VLCVideoLayout;
 
 
 import com.hamal.egg.databinding.FragmentVideoBinding;
 import java.io.File;
+import java.util.ArrayList;
 
 public class PlayerFragment extends Fragment {
     private FragmentVideoBinding binding;
     Context context;
+    private LibVLC libVlc;
+    private MediaPlayer mediaPlayer;
+    private VLCVideoLayout videoLayout;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -27,8 +32,7 @@ public class PlayerFragment extends Fragment {
         binding = FragmentVideoBinding.inflate(inflater, container, false);
         context = getContext();
         View root = binding.getRoot();
-
-
+        videoLayout = binding.videoLayout;
         File externalFilesDir = context.getExternalFilesDir(null);
         assert externalFilesDir != null;
         File[] files = externalFilesDir.listFiles();
@@ -43,18 +47,38 @@ public class PlayerFragment extends Fragment {
     }
 
     @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }
-
-    @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        ArrayList<String> options = new ArrayList<>();
+        options.add("--no-drop-late-frames");
+        options.add("--no-skip-frames");
+        options.add("--rtsp-tcp");
+
+        libVlc = new LibVLC(requireContext(), options);
+        mediaPlayer = new MediaPlayer(libVlc);
+        mediaPlayer.attachViews(videoLayout, null, false, false);
+
+        Media media = new Media(libVlc, Uri.parse("your_video_url_here"));
+        media.setHWDecoderEnabled(true, false);
+        media.addOption(":network-caching=600");
+
+        mediaPlayer.setMedia(media);
+        media.release();
+        mediaPlayer.play();
     }
 
     @Override
-    public void onPause() {
-        super.onPause();
+    public void onStop() {
+        super.onStop();
+        mediaPlayer.stop();
+        mediaPlayer.detachViews();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+        libVlc.release();
     }
 }
