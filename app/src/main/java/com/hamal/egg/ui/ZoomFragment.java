20b8b9fd94c04bbf64529c3f60b20acf4f468842
Shai <shaivopatan@gmail.com>
2024-10-25 18:28:14 +0300

    zoom view

--- app/src/main/java/com/hamal/egg/ui/ZoomFragment.java
+++ app/src/main/java/com/hamal/egg/ui/ZoomFragment.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.hamal.egg.MainActivity;
 import com.hamal.egg.databinding.ZoomViewBinding;
 
 
@@ -22,8 +23,21 @@ public class ZoomFragment extends Fragment {
         binding = ZoomViewBinding.inflate(inflater, container, false);
         context = getContext();
         View root = binding.getRoot();
+
         return root;
     }
+    @Override
+    public void onResume() {
+        super.onResume();
+        MainActivity activity = (MainActivity) context;
+        assert activity != null;
+        binding.cam1.startPlayback(activity, ":8008", binding.cameraFrame, 640, 360);
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+        binding.cam1.stopPlayback();
+    }
 
 }
